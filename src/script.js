abdab324a591a1d05754b9d290a41fdd73f1161c
vwallajabad <vwallajabad@gmail.com>
2024-09-11 21:47:55 -0500

    Added tumor model

--- src/script.js
+++ src/script.js
@@ -1,8 +1,13 @@
-let model;
+let alzheimerModel, tumorModel;
 
 async function loadAlzheimerModel() {
-    model = await tf.loadLayersModel('Alzheimer_model/model.json');
+    alzheimerModel = await tf.loadLayersModel('Alzheimer_model/model.json');
     console.log('Alzheimer model loaded successfully');
+}
+
+async function loadTumorModel() {
+    tumorModel = await tf.loadLayersModel('Tumor_model/model.json');
+    console.log('Tumor model loaded successfully');
 }
 
 function preprocessImage(imageElement) {
@@ -33,17 +38,28 @@ async function predict() {
     
     imageElement.onload = async () => {
         let processedImage = preprocessImage(imageElement);
-        let predictions = await model.predict(processedImage);
-        let predictedClass = predictions.argMax(-1).dataSync()[0];
-        let predictionProbabilities = predictions.dataSync();
+        let alzheimerPredictions = await alzheimerModel.predict(processedImage);
+        let alzheimerpredictedClass = alzheimerPredictions.argMax(-1).dataSync()[0];
+        let alzheimerpredictionProbabilities = alzheimerPredictions.dataSync();
+   
+        let tumorpredictions = await tumorModel.predict(processedImage);
+        let tumorpredictedClass = tumorpredictions.argMax(-1).dataSync()[0];
+        let tumorpredictionProbabilities = tumorpredictions.dataSync();
+     
+        let alzheimerLabel = ["Mild Demented", "Moderate Demented", "Non-Demented", "Very Mild Demented"];
+        let tumorLabel = ["Schwannoma T2", "Tuberculoma T1", "Schwannoma T1C+", "Schwannoma T1", "Papiloma T1C+", "Papiloma T1", "Oligodendroglioma T1", "Papiloma T2", "Oligodendroglioma T2", "Oligodendroglioma T1C+", "Neurocitoma T2", "Neurocitoma T1C+", "Granuloma T2", "Meningioma T2", "Neurocitoma T1", "Meduloblastoma T1C+", "Meningioma T1", "Meduloblastoma T2", "Meduloblastoma T1", "Meningioma T1C+", "Glioblastoma T2", "Granuloma T1C+", "Granuloma T1", "Glioblastoma T1C+", "Germinoma T2", "Germinoma T1C+", "Ganglioglioma T1", "Ganglioglioma T1C+", "Ependimoma T2", "Ganglioglioma T2", "Germinoma T1", "Glioblastoma T1", "Ependimoma T1C+", "Ependimoma T1", "Carcinoma T1C+", "Carcinoma T2", "Tuberculoma T1C+", "Astrocitoma T2", "Astrocitoma T1", "Tuberculoma T2", "_NORMAL T2", "Carcinoma T1", "_NORMAL T1", "Astrocitoma T1C+"];
         
-        let labels = ["Mild Demented", "Moderate Demented", "Non-Demented", "Very Mild Demented"];
-        let confidence = (predictionProbabilities[predictedClass] * 100).toFixed(2);
+        let alzheimerconfidence = (alzheimerpredictionProbabilities[alzheimerpredictedClass] * 100).toFixed(2);
+        let tumorconfidence = (tumorpredictionProbabilities[tumorpredictedClass] * 100).toFixed(2);
         
-        document.getElementById('result').innerText = `Prediction: ${labels[predictedClass]} (${confidence}%)`;
+        document.getElementById('result').innerText = `
+            Prediction: ${alzheimerLabel[alzheimerpredictedClass]} (${alzheimerconfidence}%) 
+            Prediction: ${tumorLabel[tumorpredictedClass]} (${tumorconfidence}%) 
+        `;
     };
 }
 
 window.onload = async () => {
     await loadAlzheimerModel();
+    await loadTumorModel();
 };
